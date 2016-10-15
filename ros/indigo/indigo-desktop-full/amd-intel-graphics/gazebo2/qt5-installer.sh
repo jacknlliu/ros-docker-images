e496e2d5143bc7db962a736689b8c9a020c36a35
Jack Liu <jacknlliu@gmail.com>
2016-10-15 10:42:53 +0800

    try to fix qt5 install

--- ros/indigo/indigo-desktop-full/amd-intel-graphics/gazebo2/qt5-installer.sh
+++ ros/indigo/indigo-desktop-full/amd-intel-graphics/gazebo2/qt5-installer.sh
@@ -4,31 +4,37 @@
 
 # Install Qt5.7.0
 # Dependencies: wget
-mkdir /install_data && cd /install_data
+mkdir /install_data
 
 # download qt5.7.0 opensource source package
-wget  http://download.qt.io/archive/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.7z
+cd /install_data && wget  http://download.qt.io/archive/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.7z
 
 # install dependencies 7z, libxcb
 apt-get install -y p7zip-full "^libxcb.*" libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev
 
 # compile and install qt5.7.0
-7za x qt-everywhere-opensource-src-5.7.0.7z
+cd /install_data && 7za x /install_data/qt-everywhere-opensource-src-5.7.0.7z
 mkdir -p /opt/Qt/Qt5.7.0
 export QT5PREFIX=/opt/Qt/Qt5.7.0
-./configure -prefix $QT5PREFIX -opensource -confirm-license -system-sqlite
-make
-make install
+echo "[DEBUG] QT5PREFIX: $QT5PREFIX"
+echo "[DEBUG] Current Path: $(pwd)"
+cd /install_data/qt-everywhere-opensource-src-5.7.0 && ./configure -prefix $QT5PREFIX -opensource -confirm-license -system-sqlite \
+&& make \
+&& make install
 
 # ln /usr/bin/<qt_exec>
 export QT5BINDIR=$QT5PREFIX/bin
+
 for file in moc uic rcc qmake lconvert lrelease lupdate; do
   ln -sfrvn $QT5BINDIR/$file /usr/bin/$file
+  echo "[DEBUG] ln -sfrvn $QT5BINDIR/$file /usr/bin/$file"
 done
 
 # setup ld.so.conf
 echo "$QT5PREFIX/lib" >> /etc/ld.so.conf
 ldconfig
+# added for DEBUG
+cat /etc/ld.so.conf
 
 # setup environment in /etc/profile.d
 cat > /etc/profile.d/qt5.sh << "EOF"
@@ -44,17 +50,19 @@ export QT5DIR
 # End /etc/profile.d/qt5.sh
 EOF
 
+# added for DEBUG
+cat /etc/profile.d/qt5.sh
 
 ### start to install Qtcreator 4.x ###
-cd /install_data
-
 # download qtcreator source 4.1.0
-wget http://download.qt.io/official_releases/qtcreator/4.1/4.1.0/qt-creator-opensource-src-4.1.0.tar.xz
+cd /install_data && wget http://download.qt.io/official_releases/qtcreator/4.1/4.1.0/qt-creator-opensource-src-4.1.0.tar.xz
 
-tar -xf qt-creator-opensource-src-4.1.0.tar.xz
+cd /install_data && tar -xf qt-creator-opensource-src-4.1.0.tar.xz
 
 # prepare the Makefile using qmake
-mkdir qt-creator-build && cd qt-creator-build/ && qmake -r ../qt-creator-opensource-src-4.1.0/qtcreator.pro
+cd /install_data && mkdir qt-creator-build  \
+&& cd qt-creator-build/                     \
+&& qmake -r ../qt-creator-opensource-src-4.1.0/qtcreator.pro
 
 # make
 make -j 8
@@ -67,7 +75,7 @@ make install INSTALL_ROOT=$INSTALL_DIRECTORY
 
 # ln /usr/bin/qtcreator
 ln -sf $INSTALL_DIRECTORY/bin/qtcreator   /usr/bin/qtcreator
-
+echo "[DEBUG] ln -sf $INSTALL_DIRECTORY/bin/qtcreator   /usr/bin/qtcreator"
 
 ### End installation ###
 
