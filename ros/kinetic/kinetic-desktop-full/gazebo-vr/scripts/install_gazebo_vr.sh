267ab9a96921f64e43510a3c7ed588e6f12d2acd
Jack Liu <jacknlliu@gmail.com>
2017-10-06 16:24:32 +0800

    cloud_debug(ros): add gazebo OculusVR support image

---
+++ ros/kinetic/kinetic-desktop-full/gazebo-vr/scripts/install_gazebo_vr.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+apt-get install -y --no-install-recommends libusb-dev libudev-dev libxinerama-dev
+cd /opt && hg clone https://bitbucket.org/osrf/oculussdk
+cd oculussdk
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+make
+make install
+cp ../LibOVR/90-oculus.rules /etc/udev/rules.d/
+udevadm control --reload-rules
