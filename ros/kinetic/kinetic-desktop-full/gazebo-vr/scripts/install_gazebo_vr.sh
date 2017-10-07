2d4bb7bcf217b3ecff7b7c76f79096a41a674967
Jack Liu <jacknlliu@gmail.com>
2017-10-07 14:45:32 +0800

    cloud_debug(ros): add gazebo7 VR support

--- ros/kinetic/kinetic-desktop-full/gazebo-vr/scripts/install_gazebo_vr.sh
+++ ros/kinetic/kinetic-desktop-full/gazebo-vr/scripts/install_gazebo_vr.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 apt-get update -y && apt-get install -y --no-install-recommends libusb-dev libudev-dev libxinerama-dev
 cd /opt && hg clone https://bitbucket.org/osrf/oculussdk
@@ -17,23 +17,29 @@ cp ../LibOVR/90-oculus.rules /etc/udev/rules.d/
 # install dependencies. Should we remove previous packages from ros?
 apt-get remove -y '.*gazebo.*' '.*sdformat.*' '.*ignition-math.*' '.*ignition-msgs.*' '.*ignition-transport.*'
 
-sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
 
 wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
+
 apt-get update -y
 
 mkdir -p /opt/gazebo_vr && chmod a+rwx -R /opt/gazebo_vr
 
-wget https://bitbucket.org/osrf/release-tools/raw/default/jenkins-scripts/lib/dependencies_archive.sh -O /opt/gazebo_vr/dependencies.sh
+ wget https://bitbucket.org/osrf/release-tools/raw/default/jenkins-scripts/lib/dependencies_archive.sh -O /opt/gazebo_vr/dependencies.sh
 
 ROS_DISTRO=kinetic . /opt/gazebo_vr/dependencies.sh
 
 apt-get install -y --no-install-recommends $(sed 's:\\ ::g' <<< $BASE_DEPENDENCIES) $(sed 's:\\ ::g' <<< $GAZEBO_BASE_DEPENDENCIES)
 
+apt-get install -y --no-install-recommends libbullet-dev protobuf-compiler libignition-math2-dev libignition-transport-dev libqwt-dev sdformat-sdf qt5-default
+
+# libtinyxml2-2v5 libtinyxml2-dev
+
+echo "start to build gazebo"
 
 # build and install gazebo
 hg clone https://bitbucket.org/osrf/gazebo /opt/gazebo_vr/gazebo
-cd /opt/gazebo_vr/gazebo
+cd /opt/gazebo_vr/gazebo && hg up gazebo7
 
 mkdir build
 cd build
@@ -43,5 +49,7 @@ cmake ../
 make -j4
 
 make install
+
+echo "start to make test"
 
 make tests
