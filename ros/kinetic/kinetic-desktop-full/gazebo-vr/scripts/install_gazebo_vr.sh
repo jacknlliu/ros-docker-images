0685f1eab229909f45f74b8891bcbd8dc5fcbf01
Jack Liu <jacknlliu@gmail.com>
2017-10-13 17:27:12 +0800

    cloud_debug(gazebo-vr): add oculus support for gazebo

--- ros/kinetic/kinetic-desktop-full/gazebo-vr/scripts/install_gazebo_vr.sh
+++ ros/kinetic/kinetic-desktop-full/gazebo-vr/scripts/install_gazebo_vr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update -y && apt-get install -y --no-install-recommends libusb-dev libudev-dev libxinerama-dev
+apt-get update -y && apt-get install -y --no-install-recommends mercurial  libusb-dev libudev-dev libxinerama-dev
 cd /opt && hg clone https://bitbucket.org/osrf/oculussdk
 cd oculussdk
 mkdir build
@@ -11,10 +11,13 @@ make install
 cp ../LibOVR/90-oculus.rules /etc/udev/rules.d/
 # udevadm control --reload-rules
 
+rm -rf /opt/oculussdk/build
+
 
 # recompile gazebo
 
 # install dependencies. Should we remove previous packages from ros?
+# if we remove gazebo, then the ros-desktop-full will be removed, we cannot use ros!
 apt-get remove -y '.*gazebo.*' '.*sdformat.*' '.*ignition-math.*' '.*ignition-msgs.*' '.*ignition-transport.*'
 
 echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
@@ -25,21 +28,24 @@ apt-get update -y
 
 mkdir -p /opt/gazebo_vr && chmod a+rwx -R /opt/gazebo_vr
 
- wget https://bitbucket.org/osrf/release-tools/raw/default/jenkins-scripts/lib/dependencies_archive.sh -O /opt/gazebo_vr/dependencies.sh
+wget https://bitbucket.org/osrf/release-tools/raw/default/jenkins-scripts/lib/dependencies_archive.sh -O /opt/gazebo_vr/dependencies.sh
 
-ROS_DISTRO=kinetic . /opt/gazebo_vr/dependencies.sh
+export DISTRO="xenial"
+export SDFORMAT_MAJOR_VERSION=4
+export GAZEBO_MAJOR_VERSION=7
+export ROS_DISTRO=kinetic
+
+. /opt/gazebo_vr/dependencies.sh
 
 apt-get install -y --no-install-recommends $(sed 's:\\ ::g' <<< $BASE_DEPENDENCIES) $(sed 's:\\ ::g' <<< $GAZEBO_BASE_DEPENDENCIES)
 
-apt-get install -y --no-install-recommends libbullet-dev protobuf-compiler libignition-math2-dev libignition-transport-dev libqwt-dev sdformat-sdf qt5-default
+apt-get install -y --no-install-recommends libbullet-dev protobuf-compiler libignition-math2-dev libignition-transport-dev libqwt-dev libsdformat4 sdf  sdformat-sdf libtinyxml2-2v5 libtinyxml2-dev libopenal1 libopenal-dev
 
-# libtinyxml2-2v5 libtinyxml2-dev
 
 echo "start to build gazebo"
 
 # build and install gazebo
-hg clone https://bitbucket.org/osrf/gazebo /opt/gazebo_vr/gazebo
-cd /opt/gazebo_vr/gazebo && hg up gazebo7
+hg clone https://bitbucket.org/jacknlliu/gazebo /opt/gazebo_vr/gazebo && cd /opt/gazebo_vr/gazebo && hg up gazebo7-fix-boost-compile-error
 
 mkdir build
 cd build
@@ -53,3 +59,11 @@ make install
 echo "start to make test"
 
 make tests
+
+
+# clean
+rm -rf /opt/gazebo_vr/gazebo
+
+rm -rf /etc/apt/sources.list.d/gazebo-stable.list
+
+apt-get update -y
