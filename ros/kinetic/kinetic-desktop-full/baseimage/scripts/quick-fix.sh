89edb6b66956ddd34022002efefa447a689c3885
Jack Liu <jacknlliu@gmail.com>
2019-06-12 08:42:56 +0800

    fix OpenSSL AttributeError from pip2 install packages

--- ros/kinetic/kinetic-desktop-full/baseimage/scripts/quick-fix.sh
+++ ros/kinetic/kinetic-desktop-full/baseimage/scripts/quick-fix.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pip2 install -U pyOpenSSL # for OpenSSL.ssl import AttributeError
+
 # fix moveit error, according to https://github.com/ros-planning/moveit/issues/86
 pip2 uninstall -y pyassimp
 
